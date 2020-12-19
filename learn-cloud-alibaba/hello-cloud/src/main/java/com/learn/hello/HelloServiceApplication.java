ecc8121db4c8e8b4a79a3090f331c0bbc718ef09
文松 <ws_hawk@qq.com>
2020-12-19 17:41:44 +0800

    feat: author 更新

--- learn-cloud-alibaba/hello-cloud/src/main/java/com/learn/hello/HelloServiceApplication.java
+++ learn-cloud-alibaba/hello-cloud/src/main/java/com/learn/hello/HelloServiceApplication.java
@@ -2,14 +2,16 @@ package com.learn.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * @author A80068
+ * @author hawk
  * @package com.learn.hello
  * @desc TODO
  * @date 2020/12/18
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HelloServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloServiceApplication.class, args);
--- learn-cloud-alibaba/hello-cloud/src/main/java/com/learn/hello/controller/HelloCloudController.java
+++ learn-cloud-alibaba/hello-cloud/src/main/java/com/learn/hello/controller/HelloCloudController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author A80068
+ * @author hawk
  * @package com.learn.hello.controller
  * @desc TODO
  * @date 2020/12/19
